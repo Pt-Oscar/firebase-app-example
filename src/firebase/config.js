051132c281431ba463bc8c58ffec5d2d10c17dd7
Oscar Garcia Juarez <ogarcia@pleedtech.com>
2020-09-02 17:28:57 -0500

    fist commit

---
+++ src/firebase/config.js
@@ -0,0 +1,16 @@
+const config = {
+    apiKey: "AIzaSyA4gy5gzLrFrps0x0S9lIeuF3ML5iaqcqE",
+    authDomain: "example-firebase-react-a3c21.firebaseapp.com",
+    databaseURL: "https://example-firebase-react-a3c21.firebaseio.com",
+    projectId: "example-firebase-react-a3c21",
+    storageBucket: "example-firebase-react-a3c21.appspot.com",
+    messagingSenderId: "601053545140",
+    appId: "1:601053545140:web:4a66a64de948190714cde1",
+    measurementId: "G-Q7T2B0B23H",
+    "rules": {
+      ".read": true,
+      ".write": true
+    }
+  };
+
+  export {config}
